7342d8ee16488ba33308c6cd7078df490b326fa8
zicros <zilongz@gmail.com>
2013-12-27 22:09:56 -0800

    Initial submition with basic file skeleton

---
+++ main.c
@@ -0,0 +1,16 @@
+///
+/// Summary:
+///     This project creates a firmware for the Arduino board.
+///	    The firmware simulates a USB mouse.
+///
+/// Author:
+///     Robert Zhao
+///
+
+#include <avr/pgmspace.h>   /* required by usbdrv.h */
+#include "v-usb/usbdrv/usbdrv.h"
+
+int __attribute__((noreturn)) main(void)
+{
+    
+}
